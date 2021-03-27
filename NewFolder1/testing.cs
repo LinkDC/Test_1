a7805454242cf1013b3d4a4b11b96920fd636415
LinkDC <tim.vikulov@gmail.com>
2021-03-27 16:37:57 +0300

    Initial commit
    Первый коммит с кодом

---
+++ NewFolder1/testing.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ * Код создан в рамках обучения на образовательной площадке "Цифра"
+ * 
+ * Автор - 
+ * Проект - 
+ * Класс - 
+ * Дата -
+ */
+namespace test
+{
+    class testing
+    {
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace test
+{
+    class Program
+    {
+       
+        class Programе
+        {
+            static void Main(string[] args)
+            {
+                int x, y;
+                x = 8;
+                y = 10;
+
+                if (x - y > 0)
+                {
+                    x -= y;
+                }
+                else
+                {
+                    x = 0;
+                }
+               
+                Console.WriteLine(x);
+
+            }
+        }
+    }
+}
